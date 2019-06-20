cc6e7b00672d2797567bb04b670cbd5284314652
Meunier Jérémy (DI) <jeremy.meunier@etat.ge.ch>
2019-06-20 12:32:09 +0200

    no message

---
+++ src/plsql/XXEGE_PURGE_JOB.sql
@@ -0,0 +1,12 @@
+begin
+dbms_scheduler.create_job
+(
+ job_name        => 'ESB_PURGE',
+ job_type        => 'PLSQL_BLOCK',
+ job_action      => 'BEGIN xxege_purge.run_purge(100); END;',
+ start_date      => systimestamp,
+ repeat_interval => 'freq=weekly; byday=sun; byhour=5; byminute=0; bysecond=0;',
+ enabled         => FALSE,
+ comments        => 'ESB Purge'
+);
+end;
